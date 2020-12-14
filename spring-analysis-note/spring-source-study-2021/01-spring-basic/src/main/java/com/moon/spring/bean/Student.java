1e30b2e9de1c3b59debd64c9e9fb3643fd11f19c
MooNkirA <lje888@126.com>
2020-12-14 19:42:18 +0800

    feat: 初始化2021年版本源码学习的示例工程

---
+++ spring-analysis-note/spring-source-study-2021/01-spring-basic/src/main/java/com/moon/spring/bean/Student.java
@@ -0,0 +1,18 @@
+package com.moon.spring.bean;
+
+import lombok.Data;
+
+/**
+ * Bean 实体类
+ *
+ * @author MooNkirA
+ * @version 1.0
+ * @date 2020-12-13 23:13
+ * @description
+ */
+@Data
+public class Student {
+
+    private String username = "MooN";
+
+}
---
+++ spring-analysis-note/spring-source-study-2021/01-spring-basic/src/test/java/com/moon/spring/test/SpringBasicTest.java
@@ -0,0 +1,24 @@
+package com.moon.spring.test;
+
+import com.moon.spring.bean.Student;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Spring 框架基础使用
+ *
+ * @author MooNkirA
+ * @version 1.0
+ * @date 2020-12-13 23:14
+ * @description
+ */
+public class SpringBasicTest {
+
+    @Test
+    public void testBasic() {
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        Student bean = applicationContext.getBean(Student.class);
+        System.out.println(bean.getUsername());
+    }
+
+}
